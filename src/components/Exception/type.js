a732e39ef608f37a8719eec03e6d1d4de3dfb65e
free pan <sd4015700@126.com>
2019-08-12 23:13:17 +0800

    修改: 1. 完善README

--- src/components/Exception/type.js
+++ src/components/Exception/type.js
@@ -1,16 +1,16 @@
 const types = {
   403: {
-    img: '/pic/403.svg',
+    img: process.env.VUE_APP_API_BASE_URL + '/pic/403.svg',
     title: '403',
     desc: '抱歉，你无权访问该页面'
   },
   404: {
-    img: '/pic/404.svg',
+    img: process.env.VUE_APP_API_BASE_URL + '/pic/404.svg',
     title: '404',
     desc: '抱歉，你访问的页面不存在或仍在开发中'
   },
   500: {
-    img: '/pic/500.svg',
+    img: process.env.VUE_APP_API_BASE_URL + '/pic/500.svg',
     title: '500',
     desc: '抱歉，服务器出错了'
   }
