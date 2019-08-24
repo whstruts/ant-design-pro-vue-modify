1125830009b6c4768e22ff09a53148da25f487d5
free pan <sd4015700@126.com>
2019-08-24 20:37:29 +0800

    修复: 在正式环境启用动态主题切换的功能, 但由于是非根目录, 所以导致主题css文件加载不到, 然后无法实现动态主题切换, 以及一进入主页就一直显示"主题切换中提示"

--- src/components/SettingDrawer/themeColor.js
+++ src/components/SettingDrawer/themeColor.js
@@ -15,7 +15,8 @@ export default {
     var options = {
       newColors: this.getAntdSerials(newColor), // new colors array, one-to-one corresponde with `matchColors`
       changeUrl (cssUrl) {
-        console.log('css的url地址:', process.env.VUE_APP_PROJECT_NAME + `/${cssUrl}`)
+        console.log('动态主题css的url地址:', process.env.VUE_APP_PROJECT_NAME + `/${cssUrl}`)
+        // TODO 如果是非根目录发布,且需要动态主题切换的功能, 这里就必须指定项目名, 否则会无法加载到主题css文件, 导致最终出错
         return process.env.VUE_APP_PROJECT_NAME + `/${cssUrl}` // while router is not `hash` mode, it needs absolute path
       }
     }
